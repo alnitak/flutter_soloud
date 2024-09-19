2deaccad13a024e82b85ed633106d61c8ee51f79
Marco Bavagnoli <marco.bavagnoli@gmail.com>
2024-09-19 08:20:07 +0200

    update waveform example

--- example/lib/waveform/waveform.dart
+++ example/lib/waveform/waveform.dart
@@ -1,7 +1,30 @@
+import 'dart:developer' as dev;
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_soloud/flutter_soloud.dart';
+import 'package:logging/logging.dart';
 
+/// Waveform example.
+///
 void main() {
+  // The `flutter_soloud` package logs everything
+  // (from severe warnings to fine debug messages)
+  // using the standard `package:logging`.
+  // You can listen to the logs as shown below.
+  Logger.root.level = kDebugMode ? Level.FINE : Level.INFO;
+  Logger.root.onRecord.listen((record) {
+    dev.log(
+      record.message,
+      time: record.time,
+      level: record.level.value,
+      name: record.loggerName,
+      zone: record.zone,
+      error: record.error,
+      stackTrace: record.stackTrace,
+    );
+  });
+
   runApp(const MyApp());
 }
 
@@ -11,7 +34,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter SoLoud waveform demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -31,6 +54,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  WaveForm waveForm = WaveForm.sin;
+  bool superWave = false;
+  double scale = 1;
+  double detune = 0.5;
   double frequency = 1000;
   AudioSource? currentSound;
   SoundHandle? soundHandle;
@@ -56,17 +83,18 @@ class _MyHomePageState extends State<MyHomePage> {
       currentSound =
           await SoLoud.instance.loadWaveform(WaveForm.sin, true, 1, 0);
 
-      soundHandle = await SoLoud.instance.play(
-        currentSound!,
-        loopingStartAt: const Duration(seconds: 5),
-        looping: true,
-      );
+      soundHandle = await SoLoud.instance.play(currentSound!);
 
-      setState(() {
-        isPlaying = true;
-      });
+      if (context.mounted) {
+        setState(() {
+          isPlaying = true;
+        });
+      }
 
+      SoLoud.instance.setWaveformSuperWave(currentSound!, superWave);
       SoLoud.instance.setWaveformFreq(currentSound!, frequency);
+      SoLoud.instance.setWaveformDetune(currentSound!, detune);
+      SoLoud.instance.setWaveformScale(currentSound!, scale);
     } catch (e) {
       debugPrint('Error while trying to play sound: $e');
     }
@@ -92,39 +120,127 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Freequncy : $frequency'),
-            Slider(
-              value: frequency,
-              min: 20,
-              max: 16000,
-              onChanged: (value) {
-                setState(() {
-                  frequency = value;
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('SuperWave : $superWave'),
+              Switch(
+                value: superWave,
+                onChanged: (value) {
+                  setState(() {
+                    superWave = value;
+                    if (currentSound != null && isPlaying) {
+                      SoLoud.instance
+                          .setWaveformSuperWave(currentSound!, value);
+                    }
+                  });
+                },
+              ),
+              const SizedBox(height: 20),
+              Text('Scale : ${scale.toStringAsFixed(2)}'),
+              Slider(
+                value: scale,
+                max: 4,
+                onChanged: !superWave
+                    ? null
+                    : (value) {
+                        setState(() {
+                          scale = value;
+                          if (currentSound != null && isPlaying) {
+                            SoLoud.instance
+                                .setWaveformScale(currentSound!, value);
+                          }
+                        });
+                      },
+                label: 'Scale: ${scale.toStringAsFixed(2)}',
+                activeColor: Colors.green,
+                inactiveColor: Colors.green[100],
+              ),
+              const SizedBox(height: 20),
+              Text('Detune : ${detune.toStringAsFixed(2)}'),
+              Slider(
+                value: detune,
+                max: 2,
+                onChanged: !superWave
+                    ? null
+                    : (value) {
+                        setState(() {
+                          detune = value;
+                          if (currentSound != null && isPlaying) {
+                            SoLoud.instance
+                                .setWaveformDetune(currentSound!, value);
+                          }
+                        });
+                      },
+                label: 'Detune: ${detune.toStringAsFixed(2)}',
+                activeColor: Colors.green,
+              ),
+              const SizedBox(height: 20),
+              Text('Freequncy Hz: ${frequency.toInt()}'),
+              Slider(
+                value: frequency,
+                min: 20,
+                max: 16000,
+                onChanged: (value) {
+                  setState(() {
+                    frequency = value;
 
-                  if (currentSound != null && isPlaying) {
-                    SoLoud.instance.setWaveformFreq(currentSound!, value);
-                  }
-                });
-              },
-              label: 'Frequency: ${frequency.toStringAsFixed(0)} Hz',
-              activeColor: Colors.green,
-              inactiveColor: Colors.green[100],
-            ),
-            const SizedBox(height: 50),
-            ElevatedButton(
-              onPressed: isPlaying ? null : () => play(frequency),
-              child: const Text('Play'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: isPlaying ? stop : null,
-              child: const Text('Stop'),
-            ),
-          ],
+                    if (currentSound != null && isPlaying) {
+                      SoLoud.instance.setWaveformFreq(currentSound!, value);
+                    }
+                  });
+                },
+                label: 'Frequency: ${frequency.toInt()} Hz',
+                activeColor: Colors.green,
+                inactiveColor: Colors.green[100],
+              ),
+              const SizedBox(height: 50),
+
+              /// All waveform types.
+              Wrap(
+                runSpacing: 4,
+                spacing: 4,
+                alignment: WrapAlignment.center,
+                children: [
+                  for (var i = 0; i < WaveForm.values.length; i++)
+                    OutlinedButton(
+                      onPressed: () {
+                        setState(() {
+                          waveForm = WaveForm.values[i];
+                          SoLoud.instance.setWaveform(currentSound!, waveForm);
+                        });
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all(
+                          waveForm == WaveForm.values[i]
+                              ? Colors.green
+                              : Colors.transparent,
+                        ),
+                      ),
+                      child: Text(WaveForm.values[i].name),
+                    ),
+                ],
+              ),
+              const SizedBox(height: 50),
+              Align(
+                child: OutlinedButton(
+                  onPressed: isPlaying ? null : () => play(frequency),
+                  child: const Text('Play'),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Align(
+                child: OutlinedButton(
+                  onPressed: isPlaying ? stop : null,
+                  child: const Text('Stop'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
