687934b4fdc137b93ef0a5b38415ba8fee0141a4
Marco Bavagnoli <marco.bavagnoli@gmail.com>
2024-11-13 23:06:38 +0100

    commented `buffer_stream/websocket.dat` example

--- example/lib/buffer_stream/websocket.dart
+++ example/lib/buffer_stream/websocket.dart
@@ -11,6 +11,16 @@ import 'package:flutter_soloud_example/buffer_stream/ui/buffer_widget.dart';
 import 'package:logging/logging.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
+/// This example shows how to use BufferStream with a websocket.
+/// 
+/// You must have a server which streams audio data. Here a repo that uses
+/// websocketd as a server and ffmpeg to provide audio data:
+/// https://github.com/alnitak/websocketd
+/// 
+/// Run it and choose which audio format you want to stream and the speed for 
+/// testing BufferStream buffering.
+/// Then run this example and choose the same audio format.
+
 void main() async {
   // The `flutter_soloud` package logs everything
   // (from severe warnings to fine debug messages)
