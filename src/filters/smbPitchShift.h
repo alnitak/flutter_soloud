5af838319f7311f285bb7de82a02dd5166d2bdb4
Marco Bavagnoli <marco.bavagnoli@gmail.com>
2025-12-05 18:56:36 +0100

    the pitchshift filter is now using pffft

--- src/filters/smbPitchShift.h
+++ src/filters/smbPitchShift.h
@@ -2,36 +2,47 @@
 #define SMB_PITCHSHIFT_H
 // http://blogs.zynaptiq.com/bernsee/pitch-shifting-using-the-ft/
 
-class CSmbPitchShift
-{
-    enum { MAX_FRAME_LENGTH = 8192 };
+#include "../pffft/pffft.h"
+
+class CSmbPitchShift {
+  enum { MAX_FRAME_LENGTH = 8192 };
 
 public:
-    CSmbPitchShift();
-    
-    void reset()
-    {
-        gRover = 0;
-        gInit = false;
-    }
-    void smbPitchShift(float pitchShift, long numSampsToProcess, long fftFrameSize, long osamp, float sampleRate, float *indata, float *outdata);
+  CSmbPitchShift();
+  ~CSmbPitchShift();
+
+  void reset() {
+    gRover = 0;
+    gInit = false;
+  }
+  void smbPitchShift(float pitchShift, long numSampsToProcess,
+                     long fftFrameSize, long osamp, float sampleRate,
+                     float *indata, float *outdata);
 
 private:
-    float gInFIFO[MAX_FRAME_LENGTH];
-    float gOutFIFO[MAX_FRAME_LENGTH];
-    float gFFTworksp[2 * MAX_FRAME_LENGTH];
-    float gLastPhase[MAX_FRAME_LENGTH / 2 + 1];
-    float gSumPhase[MAX_FRAME_LENGTH / 2 + 1];
-    float gOutputAccum[2 * MAX_FRAME_LENGTH];
-    float gAnaFreq[MAX_FRAME_LENGTH];
-    float gAnaMagn[MAX_FRAME_LENGTH];
-    float gSynFreq[MAX_FRAME_LENGTH];
-    float gSynMagn[MAX_FRAME_LENGTH];
+  void initFFT(long fftSize);
 
-    float gErrors[MAX_FRAME_LENGTH];
+  float gInFIFO[MAX_FRAME_LENGTH];
+  float gOutFIFO[MAX_FRAME_LENGTH];
+  float gFFTworksp[2 * MAX_FRAME_LENGTH];
+  float gLastPhase[MAX_FRAME_LENGTH / 2 + 1];
+  float gSumPhase[MAX_FRAME_LENGTH / 2 + 1];
+  float gOutputAccum[2 * MAX_FRAME_LENGTH];
+  float gAnaFreq[MAX_FRAME_LENGTH];
+  float gAnaMagn[MAX_FRAME_LENGTH];
+  float gSynFreq[MAX_FRAME_LENGTH];
+  float gSynMagn[MAX_FRAME_LENGTH];
 
-    long gRover = 0;
-    bool gInit = false;
+  float gErrors[MAX_FRAME_LENGTH];
+
+  long gRover = 0;
+  bool gInit = false;
+
+  // PFFFT members
+  PFFFT_Setup *mFFTSetup = nullptr;
+  float *mFFTWork = nullptr;
+  float *mFFTTemp = nullptr;
+  long mCurrentFFTSize = 0;
 };
 
 #endif
