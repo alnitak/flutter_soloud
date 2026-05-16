59ced1d9e5bf42244d10cbe44ae3668dccccd0cc
coolswood <coolswood@yandex.ru>
2026-05-16 23:29:36 +0300

    Fix iOS SPM miniaudio duplicate symbols

--- ios/flutter_soloud/Sources/flutter_soloud/flutter_soloud.mm
+++ ios/flutter_soloud/Sources/flutter_soloud/flutter_soloud.mm
@@ -1,3 +1,5 @@
 // Relative import to be able to reuse the C sources.
 // See the comment in the podspec for more information.
+#define MA_API static
+#include "miniaudio_objc_prefix.h"
 #include "../../../../src/flutter_soloud.cpp"
