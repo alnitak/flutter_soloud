7f28f98685448b1f1478c5ab5b91204de98fa7d6
Marco Bavagnoli <marco.bavagnoli@gmail.com>
2026-05-24 17:06:35 +0200

    fix build error using SPM with old Xcode fix #471

--- macos/flutter_soloud/Package.swift
+++ macos/flutter_soloud/Package.swift
@@ -30,14 +30,14 @@ var baseCSettings: [CSetting] = [
     .headerSearchPath("../../include"),
     .headerSearchPath("src"),
     .headerSearchPath("src/soloud/include"),
-    .unsafeFlags(["-O3", "-ffast-math"]),
+    .unsafeFlags(["-O3"]),
 ]
 
 var baseCXXSettings: [CXXSetting] = [
     .headerSearchPath("../../include"),
     .headerSearchPath("src"),
     .headerSearchPath("src/soloud/include"),
-    .unsafeFlags(["-O3", "-ffast-math"]),
+    .unsafeFlags(["-O3"]),
 ]
 
 // Add Xiph include paths only when not disabled
