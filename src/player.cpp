2f54f2018e8461217cda67c0de98d431c262c50f
Marco Bavagnoli <marco.bavagnoli@gmail.com>
2026-06-15 22:13:53 +0200

    trying with more ms of delay

--- src/player.cpp
+++ src/player.cpp
@@ -776,7 +776,7 @@ void Player::setPause(unsigned int handle, bool pause)
 void Player::pauseEngine()
 {
     std::thread([this]() {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
         if (!mInited)
             return;
         if (soloud.getActiveVoiceCount() == 0)
