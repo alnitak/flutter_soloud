a2c9a0a5a129c9b85acb50495ded51b5c19e4acb
Marco Bavagnoli <marco.bavagnoli@gmail.com>
2026-09-26 12:52:39 +0200

    update CHANGELOG

--- hook/build.dart
+++ hook/build.dart
@@ -171,13 +171,13 @@ void main(List<String> args) async {
           '-msse3',
         ],
       ],
-if (os != OS.windows) ...[
-  '-fvisibility=hidden',
-  '-Wno-unused-command-line-argument',
-  '-fsigned-char',
-  '-Wno-vla',
-  '-Wno-vla-cxx-extension',
-],
+      if (os != OS.windows) ...[
+        '-fvisibility=hidden',
+        '-Wno-unused-command-line-argument',
+        '-fsigned-char',
+        '-Wno-vla',
+        '-Wno-vla-cxx-extension',
+      ],
       if (os == OS.linux && arch == Architecture.x64) ...['-msse2', '-msse3'],
     ];
 
